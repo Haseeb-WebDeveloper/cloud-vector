48184a8875c27c9c94f928f51e62ff66f6ce18aa
wasif <wasifdevelopsite@gmail.com>
2025-11-07 05:44:02 -0800

    middle image and results spearks section are perfect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,8 @@ import ValueProps from "@/components/home/value-props";
 import FAQSection from "@/components/faq-section";
 import GetStartedSection from "@/components/cost-optimisation/get-started-section";
 import AnimatedSections from "@/components/animated";
-import Stats from "@/components/for-cto/stats";
 import CaseStudySection from "@/components/home/case-study-section";
 import { ChartPieDonutText } from "@/components/ui/pie-chart";
-import { PiggyBank, BarChart3, Percent, Rocket, Server, ShieldCheck, Users, TrendingUp } from "lucide-react";
   
 export default function Home() {
   const stats = [
@@ -28,56 +26,12 @@ export default function Home() {
     },
   ];
 
-  const SocialStats = [
-    {
-      value: "$362k+",
-      label: "Savings delivered",
-      icon: <PiggyBank size={40} className="text-primary" />,
-    },
-    {
-      value: "40%",
-      label: "Avg Cost Reduction results",
-      icon: <BarChart3 size={40} className="text-primary" />,
-    },
-    {
-      value: "68%",
-      label: "Max Cost Reduction results",
-      icon: <Percent size={40} className="text-primary" />,
-    },
-    {
-      value: "80+",
-      label: "Production launches",
-      icon: <Rocket size={40} className="text-primary" />,
-    },
-    {
-      value: "300+",
-      label: "AWS accounts under management",
-      icon: <Server size={40} className="text-primary" />,
-    },
-    {
-      value: "15+",
-      label: "Compliance standards",
-      icon: <ShieldCheck size={40} className="text-primary" />,
-    },
-    {
-      value: "12+",
-      label: "Years at Amazon/AWS",
-      icon: <Users size={40} className="text-primary" />,
-    },
-    {
-      value: "100%",
-      label: "ROI in 3 months",
-      icon: <TrendingUp size={40} className="text-primary" />,
-    },
-  ];
-
   return (
     <>
       <HomeHeroSection />
       {/* <ChartPieDonutText /> */}
       <ClientSectionV2 title="Proven Savings. Real Impact" stats={stats} />
       <AnimatedSections />
-      <Stats stats={SocialStats} />
       <ValueProps />
       <div>
         <div className="mx-auto px-6 lg:px-8 max-w-7xl pt-40 pb-12">
